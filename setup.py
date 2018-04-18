6870e255693648e3fe2a7e8d0531cfb15f02407f
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-04-18 14:52:48 -0300

    Adiciona setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+__version__ = '0.0.0dev0'
+
+
+setup(
+    name='classificador-lyra',
+    descripition='Classificação e análise de setenças e documentos judiciais.',
+    url='https://github.com/MinisterioPublicoRJ/classificador-lyra',
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    author='Felipe Ferreira & Rhenan Bartels',
+    license='MIT',
+    zip_safe=False
+)
