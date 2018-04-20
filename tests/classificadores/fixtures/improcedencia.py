313a60ced007eb067ce52c6184b6d9be116e428b
samambaman <felipe.gomes.ferreira@gmail.com>
2018-04-20 15:53:25 -0300

    Adicao dos testes de improcedencia

---
+++ tests/classificadores/fixtures/improcedencia.py
@@ -0,0 +1,100 @@
+improcedentes = [
+    """Trata-se de REPRESENTAÇÃO ADMINISTRATIVA ajuizada pelo MINISTÉRIO PÚBLICO 
+    DO ESTADO DO RIO DE JANEIRO em face de VERÔNICA FERREIRA DA SILVA e JORGE 
+    CRISTIANO DE AZEVEDO, genitores dos menores LAVÍNIA VICTÓRIA FERREIRA DA SILVA 
+    DE AZEVEDO e CHRISTIANO FERREIRA DA SILVA DE AZEVEDO, pela prática da infração 
+    administrativa prevista no artigo 249 do ECA, consoante petição inicial de fls. 
+    02A/02B.
+
+	Aduziu, em síntese, o Ministério Público: que os representados são omissos: que 
+    não se preocupam com os cuidados necessários à vida dos filhos; que não os levam 
+    para receber os tratamentos de saúde de que necessitam; que não seguem as 
+    orientações do Conselho Tutelar de Varre Sai; que não acompanham o desenvolvimento 
+    escolar dos filhos.
+
+	Com a inicial, os documentos de fls. 02C/29.
+
+	Regularmente citados (fl. 33), os representados não ofereceram contestação (fl. 34).
+
+	Por decisão de fl. 36, foi decretada a revelia e reiterada a determinação de 
+    realização de estudos social e psicológico.
+
+	Foi realizado estudo social, constante de fls. 38/41, cuja conclusão aponta: que as 
+    referidas crianças, antes negligenciadas em seus direitos pela genitora, atualmente
+    se encontram bem cuidadas e frequentando a escola; que a genitora abandonou o hábito 
+    etílico; que as avós paterna e materna estão empenhadas no resguardo dos direitos 
+    dos netos, demonstrando compreensão quanto à responsabilidade dos genitores, 
+    afirmando que não hesitariam em denunciar novas situações de risco.
+
+	Foi realizado, também, estudo psicolígico, constante de fls. 58/60, cuja conclusão 
+    indica: que, atualmente, não foi constatada negligência por parte da genitora no 
+    contexto familiar; que, ao que parece, a genitora conscientizou-se das consequências 
+    que o hábito etílico estava impingindo à sua vida e de seus filhos, o que a motivou 
+    a abandonar tal hábito; que as crianças estão bem cuidadas pela mãe, matriculadas na 
+    rede de ensino e com a carteira de vacinação em dia; que, emocionalmente, as 
+    crianças demonstraram felicidade ao lado da mãe.
+
+	O feito foi saneado à fl. 49.
+
+	Foi realizada AIJ à fl. 76, em que foi ouvida um dos Conselheiros Tutelares de Varre 
+    Sai, uma vez que o MP desistiu da oitiva das demais testemunhas. Em seu depoimento, 
+    a testemunha afirmou que, atualmente, a situação das crianças encontra-se 
+    regularizada e que, em visita à residência do núcleo familiar, verificou que o 
+    ambiente estava limpo e organizado, bem como que as crianças encontram-se 
+    matriculadas na escola (fl. 77). 
+
+	Às fls. 85, vê-se promoção final do Ministério Público no sentido da improcedência 
+    do pedido.
+
+	É o relatório. Decido.
+
+	O processo busca a aplicação da penalidade administrativa prevista no artigo 249 do 
+    ECA aos representados.
+
+	Certo que, no curso do processo, não foram coligidas provas suficientes para embasar 
+    a condenação dos requeridos à penalidade prevista no ECA.
+
+	Da análise das peças processuais, mormente dos estudos social e psicológico, resta 
+    claro que a situação com relação aos menores modificou-se positivamente, uma vez 
+    que as crianças estão recebendo todos os cuidados e atenção necessários ao seu 
+    regular desenvolvimento, bem como que a representada abandonou o vício do 
+    alcoolismo, este que gerava toda sorte de problemas aos filhos.
+
+	Vale, ainda, ressaltar, o depoimento da testemunha, Conselheiro Tutelar em Varre 
+    Sai, que indicou que os menores estão bem cuidados, que estão matriculados em 
+    rede de ensino, e que a residência destes estava limpa e organizada..
+
+	Por fim, cumpre salientar que o próprio Ministério Público, autor da ação, 
+    requereu a improcedência.
+
+	Feitas tais considerações, JULGO IMPROCEDENTE O PEDIDO, extinguindo o processo 
+    com resolução do mérito, na forma do artigo 487, I, do novo CPC.
+
+	Sem custas.
+
+	P.R.I. Ciência ao MP.
+
+	Transitada em julgado e observadas as formalidades/cautelas legais, dê-se baixa 
+    e arquive-se.
+
+    """
+]
+
+nao_improcedentes = [
+    """
+    Decisão
+
+    Intimem-se os requerentes para que emendem a inicial, excluindo a genitora do pólo 
+    ativo do processo e acrescentando o sobrenome a ser atribuído à criança.
+
+    Intimem-se, ainda, para cumprimento do ítem 3 da promoção ministerial.
+
+    O art. 141, §2º da Lei Federal nº 8.069/90 já prevê a gratuidade de justiça para 
+    as ações judiciais referentes à criança e ao adolescente. Sendo assim, indefiro o 
+    item 4 da promoção ministerial.
+
+    Realizem-se os estudos técnicos.
+
+    Após, voltem conclusos.
+    """
+]
---
+++ tests/classificadores/test_improcedente.py
@@ -0,0 +1,20 @@
+import operator
+
+from classificadores.regex import ImprocedenteClassifier
+
+from .fixtures.improcedencia import improcedentes, nao_improcedentes
+from .utils import RegexTest
+
+
+class ImprocedenciaTest(RegexTest):
+    classificador = ImprocedenteClassifier
+
+    def test_improcedente(self):
+        self.textos = improcedentes
+        self.operador = operator.truth
+        self.testador_textos()
+
+    def test_nao_improcedente(self):
+        self.textos = nao_improcedentes
+        self.operador = operator.not_
+        self.testador_textos()
