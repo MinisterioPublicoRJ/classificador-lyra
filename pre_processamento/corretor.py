a244796d9ee2af4febe79165ae62cb1250a3ad9b
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-04-26 10:55:05 -0300

    Utiliza palavra mais proxima para corrigir palavra errada

--- pre_processamento/corretor.py
+++ pre_processamento/corretor.py
@@ -36,21 +36,22 @@ def corrige_palavras(documento_original, palavras_importantes):
     documento_corrigido = documento_original
     documento_splitted = documento_original.split()
 
-    for palavra in PALAVRAS_IMPORTANTES:
-        for token in documento_splitted:
-            if (SequenceMatcher(None, palavra, token.lower()).ratio() >
-                    LIMIAR_SEMELHANCA):
-                n_letras = len(palavra)
-                if sum(map(lambda x: x.isupper(), token)) / n_letras > 0.5:
-                    documento_corrigido = documento_corrigido.replace(
-                        token,
-                        palavra.upper()
-                    )
-                else:
-                    documento_corrigido = documento_corrigido.replace(
-                        token,
-                        palavra
-                    )
+    for token in documento_splitted:
+        palavra_similar = encontra_palavra_similiar(
+            token.lower(),
+            palavras_importantes
+        )
+        n_letras = len(token)
+        if sum(map(lambda x: x.isupper(), token)) / n_letras > 0.5:
+            documento_corrigido = documento_corrigido.replace(
+                token,
+                palavra_similar.upper()
+            )
+        else:
+            documento_corrigido = documento_corrigido.replace(
+                token,
+                palavra_similar
+            )
 
     return documento_corrigido
 
