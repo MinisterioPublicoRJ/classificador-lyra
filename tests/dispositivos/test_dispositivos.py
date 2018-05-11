7785bc501577d0663bc17c2a7c90b1e88c4ddb7d
samambaman <felipe.gomes.ferreira@gmail.com>
2018-05-11 16:08:45 -0300

    Formatação de código

--- tests/dispositivos/test_dispositivos.py
+++ tests/dispositivos/test_dispositivos.py
@@ -8,7 +8,6 @@ class Dispositivos(TestCase):
     def test_recortar(self):
         assert Dispositivo._recortar(procedencia, 1000) == recorte_procedencia
 
-
     def test_classificaco_com_dispositivo(self):
         classificador = ProcedenteClassifier(procedencia)
         classificador.classificar()
