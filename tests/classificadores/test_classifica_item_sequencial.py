8524e8932fcbfa894f37936717394b81c482e25e
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-07-12 15:07:40 -0300

    substitui assert type por isinstance

--- tests/classificadores/test_classifica_item_sequencial.py
+++ tests/classificadores/test_classifica_item_sequencial.py
@@ -1,4 +1,3 @@
-from unittest import TestCase
 from classificador_lyra.regex import classifica_item_sequencial
 from classificador_lyra.procedencia import (
     classificadores,
@@ -7,11 +6,9 @@ from classificador_lyra.procedencia import (
 from .fixtures.improcedencia import improcedentes
 
 
-class ClassificaTest(TestCase):
-
-    def test_classificador_improcedente(self):
-        resultado = classifica_item_sequencial(
-            improcedentes[0],
-            classificadores
-        )
-        assert type(resultado['classificacao']) == ImprocedenteClassifier
+def test_classificador_improcedente():
+    resultado = classifica_item_sequencial(
+        improcedentes[0],
+        classificadores
+    )
+    assert isinstance(resultado['classificacao'], ImprocedenteClassifier)
