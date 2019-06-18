03e4f3c7d916bafbdb711f7d84ef4815347cd4e5
Felipe Ferreira <felipe.gomes.ferreira@gmail.com>
2019-06-18 16:59:57 -0300

    Formatação de

--- classificador_lyra/pre_processamento/utils.py
+++ classificador_lyra/pre_processamento/utils.py
@@ -4,13 +4,23 @@ from os import environ as env
 import nltk
 
 
-dicionario = set([p.lower().strip() for p in
-                  open(env.get('DICIONARIO', 'classificador_lyra/pre_processamento/dicionario.txt'), encoding='utf-8')])
+dicionario = set([p.lower().strip() for p in open(
+    env.get(
+        'DICIONARIO',
+        'classificador_lyra/pre_processamento/dicionario.txt'),
+    encoding='utf-8')])
 
-stopwords = [s.strip().lower() for s in open(env.get('STOPWORDS', 'classificador_lyra/pre_processamento/stopwords.txt'))]
+stopwords = [s.strip().lower() for s in open(
+    env.get(
+        'STOPWORDS',
+        'classificador_lyra/pre_processamento/stopwords.txt'))]
 
 # Prepara corpora
-corpora = [p.strip() for p in open(env.get('BAG_OF_WORDS', 'classificador_lyra/pre_processamento/corpora.txt'))]
+corpora = [p.strip() for p in open(
+    env.get(
+        'BAG_OF_WORDS',
+        'classificador_lyra/pre_processamento/corpora.txt'))]
+
 bigramas_corpora = Counter(nltk.bigrams(corpora))
 
 
